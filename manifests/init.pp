65dd5524622b59224af0b9d47e7c1fbc96c7798b
Atze de Vries <atze.devries@naturalis.nl>
2014-04-18 16:14:56 +0200

    modified ls command

--- manifests/init.pp
+++ manifests/init.pp
@@ -79,7 +79,7 @@ class common {
     exec { "extract ${package_name}" :
       command => "${extract_command} ${download_dir}/${package_name}",
       cwd     => $extract_dir,
-      unless  => "/usr/bin/test ${$extract_dir}/${package_name}",
+      unless  => "/bin/ls ${$extract_dir}/${package_name}",
       require => Exec["download ${package_name}"],
     }
   }
